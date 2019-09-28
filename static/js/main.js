96c4ddc2fd0f55353c709be83fd8fa27318e43dd
atsukoba <atsuya_wgp4346@keio.jp>
2019-09-28 12:26:39 +0900

    edit animations

--- static/js/main.js
+++ static/js/main.js
@@ -9,35 +9,33 @@ $(function() {
 });
 
 // key visual
-$(function() {
-  for (let i = 1; i < 7; i++) {
-    setTimeout(function() {
-      $(`.c_${i}`).css("opacity", .35);
-    }, 100);
-  }
-});
+// $(function() {
+//   for (let i = 1; i < 7; i++) {
+//     setTimeout(function() {
+//       $(`.c_${i}`).css("opacity", .35);
+//     }, 100);
+//   }
+// });
 
 // page background animation
 window.addEventListener("deviceorientation", handleOrientation, true);
- 
+
 function handleOrientation(event) {
   var beta     = event.beta;
   var gamma    = event.gamma;
-  var posX = 20 * gamma / 90
-  var posY = 20 * beta / 180
-  
+  var posX = 50 * gamma / 90
+  var posY = 50 * beta / 180
   $("section:not(.top)").css("background-position", `${posX} ${posY}`);
   console.log(pos)
 }
 
-// $(function() {
-//   setInterval(() => {
-//     var pos = 100 * Math.random();
-//     $("section:not(.top)").css("background-position", `${posX} ${posY}`);
-//     console.log(pos)
-//   }, 1000);
-// });
-
+$(function() {
+  setInterval(() => {
+    var pos = 100 * Math.random();
+    $("section:not(.top)").css("background-position", `${posX} ${posY}`);
+    console.log(pos)
+  }, 1000);
+});
 
 // modal window
 $(document).ready(function(){
