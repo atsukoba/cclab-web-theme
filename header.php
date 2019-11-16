2461823b1b4e41efebc0346d350248fb3a09e87f
atsukoba <atsuya_wgp4346@keio.jp>
2019-11-16 12:09:10 +0900

    add wp_head

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/curtains.min.js"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
+  <?php wp_head(); ?>
 </head>
 
 <body>
