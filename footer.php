604e53bdeb32911d13a321f7ac9eb4e094443e0c
atsukoba <atsuya_wgp4346@keio.jp>
2019-11-20 02:24:47 +0900

    added footer

--- footer.php
+++ footer.php
@@ -1,2 +1,9 @@
+<footer>
+  <p><a href="https://www.sfc.keio.ac.jp/">慶應義塾大学 湘南藤沢キャンパス</a></p>
+  <p>環境情報学部 政策・メディア研究科 徳井直生研究室</p>
+  <hr>
+  <p>© <?php echo date("Y"); ?> Computational Creativity Lab.</p>
+</footer>
 </body>
+
 </html>
