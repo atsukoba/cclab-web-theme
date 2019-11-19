c387f419dd84a396cf3dbb86eb6222640938e538
atsukoba <atsuya_wgp4346@keio.jp>
2019-11-20 02:44:42 +0900

    fixed footer contents

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <p><a href="https://www.sfc.keio.ac.jp/">慶應義塾大学 湘南藤沢キャンパス / keio University SFC</a>
+  <p><a href="https://www.sfc.keio.ac.jp/">慶應義塾大学 湘南藤沢キャンパス - Keio University SFC</a>
     <br>環境情報学部 政策・メディア研究科 徳井直生研究室
   </p>
   <hr class="sp">
