05f7293a9928c133e3ffe9fad058d6b9e0b88ec1
atsukoba <atsuya_wgp4346@keio.jp>
2019-11-20 02:01:08 +0900

    add 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<section class="post single">
+  <div class="section-inner-wrapper">
+    <div class="container">
+      <h1>404 Not Found</h1>
+      <p>お探しのページは削除されたかURLが異なる可能性があります。</p>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
